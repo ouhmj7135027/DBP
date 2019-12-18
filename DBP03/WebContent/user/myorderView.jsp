7d9096716eac7749fc4387c88e00a4f78d265651
lina2 <lina2@DESKTOP-EG708IA>
2019-12-18 22:35:19 +0900

    myorderList(order_p), myorderView(order_detail) 구현

---
+++ DBP03/WebContent/user/myorderView.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>myorderView</title>
+<style>
+ .orderView li { margin-bottom:20px; padding-bottom:20px; border-bottom:1px solid #999; }
+ .orderView li::after { content:""; display:block; clear:both; }
+ 
+ .thumb { float:left; width:200px; }
+ .thumb img { width:200px; height:200px; }
+ .gdsInfo { float:right; width:calc(100% - 220px); line-height:2; }
+ .gdsInfo span { font-size:20px; font-weight:bold; display:inline-block; width:100px; margin-right:10px; }
+</style>
+</head>
+<body>
+<section id="content">
+ <ul class="orderView">
+  <c:forEach items="${orderView}" var="orderView">     
+  <li>
+   <div class="thumb">
+   <img src="<c:url value='${orderView.imgsrc}' />">
+   </div>
+   <div class="gdsInfo">
+    <p>
+     <span>상품명</span>${orderView.p_name}<br>
+     <span>가격</span>${orderView.p_price}원<br>
+     <span>상품 개수</span>${orderView.o_amount}개<br>
+     <span>최종 가격</span>${orderView.total_price}원                  
+    </p>
+   </div>
+  </li>     
+  </c:forEach>
+ </ul>
+</section>
+</body>
+</html>
--- DBP03/src/persistence/dao/impl/order_detailDAOImpl.java
+++ DBP03/src/persistence/dao/impl/order_detailDAOImpl.java
@@ -1,17 +1,11 @@
 package persistence.dao.impl;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import persistence.DAOFactory;
 import persistence.dao.order_detailDAO;
-import persistence.dao.order_pDAO;
-import service.dto.cartDTO;
 import service.dto.order_detailDTO;
-import service.dto.order_pDTO;
-import service.dto.productDTO;
 
 public class order_detailDAOImpl implements order_detailDAO{
 	private JDBCUtil jdbcUtil = null;
@@ -20,44 +14,46 @@ public class order_detailDAOImpl implements order_detailDAO{
 		jdbcUtil = new JDBCUtil();
 	}
 	
-	/*public List<order_detailDTO> listOrder()
-	{
-		String sql = "select order_num, g.name gname, c.name cname, c.address, order_date from orderlist "
-				+ "inner join goods g on orderlist.goods_num = g.goods_num"
-				+ "inner join customer c on orderlist.cust_id = c.id order by order_num desc";
+	public List<order_detailDTO> getOrderViewById(int oid) {
+		// TODO Auto-generated method stub
+		String listQuery = "select imgsrc, order_detail.product_id AS product_id, p_name, p_price, o_amount, total_price"
+						 + " from order_detail, product"
+						 + " where order_detail.product_id = product.product_id AND order_id = ? ";
+				
 		
-		List<OrderDTO> ReturnList = new ArrayList<OrderDTO>();
+		Object[] param = new Object[] {oid};	
+		jdbcUtil.setSql(listQuery);
+		jdbcUtil.setParameters(param);
 		
 		try {
-			pstmt = con.prepareStatement(sql);
-			
-			
-			rs = pstmt.executeQuery();
-			
-			while(rs.next())
-			{
-				OrderDTO T = new OrderDTO();
-				
-				T.setCustAddr(rs.getString("c.address"));
-				T.setCustName(rs.getString("cname"));
-				T.setOrder_num(rs.getInt("order_num"));
-				T.setGoodsName(rs.getString("gname"));
-				T.setOrder_date(rs.getDate("order_date"));
-				
-				ReturnList.add(T);
+			ResultSet rs = jdbcUtil.executeQuery();
+			List<order_detailDTO> list = new ArrayList<order_detailDTO>();
+			while (rs.next()) {
+				order_detailDTO dto = new order_detailDTO();
+				dto.setImgsrc(rs.getString("imgsrc"));
+				dto.setProduct_id(rs.getInt("product_id"));
+				dto.setP_name(rs.getString("p_name"));
+				dto.setP_price(rs.getInt("p_price"));
+				dto.setO_amount(rs.getInt("o_amount"));
+				dto.setTotal_price(rs.getInt("total_price"));
+				list.add(dto);
 			}
-			
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				System.out.print("List order SQL ERROR");
-			}
-			
-			return ReturnList;
-	}*/
+			return list;
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return null;
+		
+	}
 	
 	public int insertOrder_detail(order_detailDTO ord_d) {
-		String sql = "INSERT INTO ORDER_DETAIL (order_detail_id, order_id, product_id, o_amount, total_price, review ) "
-				+ "VALUES(S_ORDER_DETAIL_ID.nextval, ?, ?, ?, ?, \'0\' )";	
+		/*String sql = "INSERT INTO ORDER_DETAIL (order_detail_id, order_id, product_id, o_amount, total_price, review ) "
+				+ "VALUES(S_ORDER_DETAIL_ID.nextval, ?, ?, ?, ?, \'0\' )";	*/
+		
+		String sql = "INSERT INTO ORDER_DETAIL (order_detail_id, order_id, product_id, o_amount, total_price ) "
+				+ "VALUES(S_ORDER_DETAIL_ID.nextval, ?, ?, ?, ?)";	
 	
 	Object[] param = new Object[] {ord_d.getOrder_id(), ord_d.getProduct_id(), ord_d.getO_amount(), ord_d.getTotal_price()};		
 	jdbcUtil.setSql(sql);
@@ -76,6 +72,7 @@ public class order_detailDAOImpl implements order_detailDAO{
 	return 0;	
 		
 	}
+	
 	public int reviewed_order(String m_id, String product_id) {
 		String sql = "INSERT INTO ORDER_DETAIL ( review ) "
 				+ "VALUES(\'0\' ) where m_id = ? and product_id = ?";			
@@ -94,31 +91,6 @@ public class order_detailDAOImpl implements order_detailDAO{
 	return 0;	
 		
 	}
-	public List<order_detailDTO> getOrderListByMid(int order_id) {
-		 
-		String sql = "select p_name " +
-						"from product,order_detail" +
-						"where product.product_id = order_detail.product_id AND order_id = ? ";
-		Object[] param = new Object[] {order_id};
-		jdbcUtil.setSqlAndParameters(sql, param);
-		
-		try {
-			ResultSet rs = jdbcUtil.executeQuery();
-			List<order_detailDTO> list = new ArrayList<order_detailDTO>();
-			while(rs.next()) {
-				order_detailDTO dto = new order_detailDTO();
-				dto.setProduct_id(rs.getInt("product_id"));
-				
-				list.add(dto);
-			}
-			return  list;
-		}catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection 반환
-		}		
-		return null;
-	}                     
 	
 	public int updateOrder_detail(order_detailDTO ord_d) {
 		String updateQuery = "update order_detail set ";
@@ -147,15 +119,15 @@ public class order_detailDAOImpl implements order_detailDAO{
 		jdbcUtil.setParameters(newParam);
 		
 		try {
-			int result = jdbcUtil.executeUpdate();		// update �눧占� 占쎈뼄占쎈뻬
-			return result;			// update 占쎈퓠 占쎌벥占쎈퉸 獄쏆꼷�겫占쎈쭆 占쎌쟿�굜遺얜굡 占쎈땾 獄쏆꼹�넎
+			int result = jdbcUtil.executeUpdate();		// update 占쎈닱�뜝占� �뜝�럥堉꾢뜝�럥六�
+			return result;			// update �뜝�럥�뱺 �뜝�럩踰ε뜝�럥�돵 �뛾�룇瑗뤄옙寃ュ뜝�럥彛� �뜝�럩�읉占쎄턀�겫�뼔援� �뜝�럥�빢 �뛾�룇瑗뱄옙�꼶
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		}
 		finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection 獄쏆꼹�넎
+			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection �뛾�룇瑗뱄옙�꼶
 		}		
 		return 0;
 	}
@@ -168,16 +140,17 @@ public class order_detailDAOImpl implements order_detailDAO{
 		jdbcUtil.setParameters(param);
 		
 		try {
-			int result = jdbcUtil.executeUpdate();		// delete �눧占� 占쎈뼄占쎈뻬
-			return result;						// delete 占쎈퓠 占쎌벥占쎈퉸 獄쏆꼷�겫占쎈쭆 占쎌쟿�굜遺얜굡 占쎈땾 獄쏆꼹�넎
+			int result = jdbcUtil.executeUpdate();		// delete 占쎈닱�뜝占� �뜝�럥堉꾢뜝�럥六�
+			return result;						// delete �뜝�럥�뱺 �뜝�럩踰ε뜝�럥�돵 �뛾�룇瑗뤄옙寃ュ뜝�럥彛� �뜝�럩�읉占쎄턀�겫�뼔援� �뜝�럥�빢 �뛾�룇瑗뱄옙�꼶
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();		
 		} finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection 獄쏆꼹�넎
+			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection �뛾�룇瑗뱄옙�꼶
 		}
 		return 0;
 	}
+
 
 }
